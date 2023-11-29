675618c5f6fa2e067949853bfb2a9d9e371c2037
Fania Everitt <me@fania.eu>
2023-11-29 14:43:20 +0000

    using iframes

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ async function startSlideshow() {
 function stopSlideshow() {
   console.log('cancelling slideshow');
   const slideURL = document.getElementById('slideshowURL');
+  mainContent.classList.remove('hide');
   if(slideURL) {
     slideURL.remove();
   }
@@ -35,9 +36,55 @@ function handleSlideshow() {
   const rand = urls[Math.floor(Math.random()*urls.length)];
   console.log('rand',rand);
 
-  const slideURL = document.getElementById('slideshowURL');
-  slideURL.content = `4;url=${rand}`;
+  // const slideURL = document.getElementById('slideshowURL');
+  // slideURL.content = `4;url=${rand}`;
+  
+  const mainContent = document.getElementsByTagName('main')[0];
+  console.log(mainContent);
+
+  const placeholder = document.getElementById('slideshowPlaceholder');
+  const ifrm = document.createElement("iframe");
+  ifrm.setAttribute("id", "slideshowPlaceholder");
+  ifrm.setAttribute("title", `Slideshow Order Style`);
+  // ifrm.setAttribute("width", `${window.innerWidth}`);
+  // ifrm.setAttribute("height", `${window.innerHeight}`);
+  ifrm.setAttribute("width", `${window.innerWidth - 7}px`);
+  ifrm.setAttribute("height", `${window.innerHeight - 7}px`);
+
+  ifrm.setAttribute("src", `${rand}`);
+  if(placeholder) { // already exists
+    placeholder.remove();
+  }
+  mainContent.classList.add('hide');
+  document.body.appendChild(ifrm);
 
 }
 
 // startSlideshow();
+
+
+
+function getScrollbarWidth() {
+
+  // Creating invisible container
+  const outer = document.createElement('div');
+  outer.style.visibility = 'hidden';
+  outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement('div');
+  outer.appendChild(inner);
+  
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+    
+}
+
+getScrollbarWidth();
